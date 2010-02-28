e675b2cb1d2933118a6f4499e44584b7c3aff229
Hugo Lopes Tavares <hltbra@gmail.com>
2010-02-27 22:08:38 -0300

    made a dummy implementation of RubyPyModule and the tests pass

--- lib/rubypython_bridge2.rb
+++ lib/rubypython_bridge2.rb
@@ -11,15 +11,42 @@ module RubyPythonBridge2
     RubyPyApi.stop
   end
 
+  def self.import(module_name)
+    RubyPythonBridge2::RubyPyModule.new module_name
+  end
+
   def self.func(module_name, func_name, *args)
-    start
-    modulepy = RubyPyApi.import module_name
     args_list = RubyPyApi::PyObject.newList(*args.map{|x| RubyPyApi::PyObject.new x})
     args_tuple = RubyPyApi::PyObject.makeTuple args_list
-    funcpy = modulepy.getAttr(func_name)
+    start
+    modulepy = import module_name
+    funcpy = modulepy.getAttr func_name
     result = funcpy.callObject args_tuple
     stop
     result.rubify
   end
 
+end
+
+
+class RubyPythonBridge2::RubyPyModule
+  def initialize(module_name)
+    @module = RubyPyApi.import module_name
+  end
+
+  def getAttr(attrname)
+    @module.getAttr attrname
+  end
+
+  def Request(*args)
+    args.empty? ? RubyPythonBridge2::RubyPyClass.new : RubyPythonBridge2::RubyPyInstance.new
+  end
+end
+
+
+class RubyPythonBridge2::RubyPyClass
+end
+
+
+class RubyPythonBridge2::RubyPyInstance
 end
